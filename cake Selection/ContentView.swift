9941d1f7b7a3d3689ab9233db0b439678986651c
Mallika <Mallika.siri2536@gmail.com>
2024-09-27 10:00:05 +0700

    Initial Commit

---
+++ cake Selection/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  cake Selection
+//
+//  Created by admin2 on 27/9/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+            Text("My bakery 🎂😇")
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundStyle(.blue)
+      
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cake Selection/cake_SelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  cake_SelectionApp.swift
+//  cake Selection
+//
+//  Created by admin2 on 27/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct cake_SelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
